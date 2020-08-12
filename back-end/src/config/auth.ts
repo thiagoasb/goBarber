47a8a4a7734a46e1e5cb4a8df29e5a8072bc84ca
Thiago Araújo <thiagoasbezerra@gmail.com>
2020-08-11 22:18:49 -0300

    problems in the commit

---
+++ back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '6728d4bef754589076c82c96aa70534d',
+    expiresIn: '1d',
+  },
+};
---
+++ gobaber-web/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <AppProvider>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </AppProvider>
+
+    <GlobalStyle />
+  </>
+);
+
+export default App;
