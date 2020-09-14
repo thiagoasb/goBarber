1b4b3b0a98530472c90ffe2bc6349074a973e999
Thiago Araújo <thiagoasbezerra@gmail.com>
2020-09-14 12:41:58 -0300

    bug: correcting reset password and ethereal bugs

--- server/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ server/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -46,6 +46,7 @@ export default class EtherealMailProvider implements IMailProvider {
       subject,
       html: await this.mailTemplateProvider.parse(templateData),
     });
+
     console.log('Message sent: %s', message.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   }
